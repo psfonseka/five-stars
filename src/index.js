00b11913082dc37355b3b91aff1a186e9ef1b404
psfonseka <psfonseka@gmail.com>
2019-09-16 19:38:23 -0400

    added input for changing five stars quarters

--- src/index.js
+++ src/index.js
@@ -8,13 +8,42 @@ import QuarterStars from './QuarterStars.js';
 class FiveStars extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      rating: 0,
+      ratingEntry: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      ratingEntry: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      rating: this.state.ratingEntry,
+      ratingEntry: ""
+    });
   }
 
   render() {
     return (
     <div>
       <DynamicStars/>
-      <QuarterStars rating={3.8}/>
+      <QuarterStars rating={this.state.rating}/>
+      <form onSubmit={this.handleSubmit}>
+      <label>
+        Rating: {this.state.rating} <br/>
+        <input type="text" name="rating" value={this.state.ratingEntry} onChange={this.handleChange}/>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
     </div>
     )
   }
